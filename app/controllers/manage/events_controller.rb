bc830a75ede506302e8efa2253ca0172391a1ec1
Karmickoala904 <karmickoala904@gmail.com>
2014-10-06 10:18:40 +0700

    crud for manage/events

---
+++ app/controllers/manage/events_controller.rb
@@ -0,0 +1,32 @@
+class Manage::EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to manage_events_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def destroy
+    Event.find(params[:id]).destroy
+    redirect_to manage_events_path
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :when)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :manage do
+    resources :events
+  end
   resources :events, only: [:index, :show]
 
   root to: 'events#index'
