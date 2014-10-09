c33a6cabf891d949d82ae58de594ea4655c4d680
Karmickoala904 <karmickoala904@gmail.com>
2014-10-09 15:35:12 +0700

    manage styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   namespace :manage do
     resources :events
+    root to: 'events#index'
   end
 
   resources :events, only: [:index, :show] do
     post 'send_event', as: :send,        on: :collection
   end
 
+  resources :participants, only: [:new, :create]
+
   get 'about',            as: :about, to: 'static_pages#about'
   get 'show_conditions',  as: :show_conditions, to: 'static_pages#show_conditions'
-
-  resources :participants, only: [:new, :create]
 
   root to: 'static_pages#about'
 end
