a725f4665c45a4013e0f40e56ff5991118badefe
PeoneEr <peoneerko@yandex.ru>
2014-10-22 12:01:56 +0700

    order

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def related(event)
     @related_items = {}.tap{ |hash|
-      event.related_items.each do |item|
+      event.related_items.order('id desc').each do |item|
         hash["#{item.item_type}_#{item.item_id}"] = JSON.parse($redis.get "#{item.item_type}_#{item.item_id}")
       end
     }
