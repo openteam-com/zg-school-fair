8f1c3455ab1deeeb18a6fe8739d8989fb03bf9ee
PeoneEr <peoneerko@yandex.ru>
2015-07-28 15:56:53 +0600

    change namespace

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => Settings['redis.url'], :namespace => 'znaigorod' }
+  config.redis = { :url => Settings['redis.url'], :namespace => 'school-fair' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => Settings['redis.url'], :namespace => 'znaigorod' }
+  config.redis = { :url => Settings['redis.url'], :namespace => 'school-fair' }
 end
